50450a9e34e0d9edffc812109320b8c69f49ebaa
sanjayrathva091 <sanjayrathva091@gmail.com>
2023-01-14 02:45:46 +0530

    modified: user model

--- models/user.model.js
+++ models/user.model.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
-    age: { type: Number, required: true },
-    location: { type: String, required: true },
-    username: { type: String, required: true },
+    firstName: { type: String },
+    lastName: { type: String },
+    age: { type: Number },
+    location: { type: String },
+    username: { type: String },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     role: { type: String, required: true }
